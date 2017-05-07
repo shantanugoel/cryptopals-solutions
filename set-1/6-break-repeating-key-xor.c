bd985fc6baf1827cdc96ec10c00e9b7dfcbcea21
Shantanu Goel <shantanu.goel@gmail.com>
2017-05-07 17:11:40 +0530

    keysize guess for 1-6

--- set-1/6-break-repeating-key-xor.c
+++ set-1/6-break-repeating-key-xor.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <limits.h>
 #include "../utils/utils.h"
+
+#define MIN_KEY_SIZE 2
+#define MAX_KEY_SIZE 40
 
 int main()
 {
-  printf("%u\n", get_hamming_distance("this is a test", "wokka wokka!!!"));
+  char string1[MAX_KEY_SIZE + 1] = {0};
+  char string2[MAX_KEY_SIZE + 1] = {0};
+  double min_distance = INT_MAX;
+  unsigned int best_keysize = MIN_KEY_SIZE;
+  FILE* file = fopen("6.txt", "r");
+  for(unsigned int keysize = 2; keysize <= MAX_KEY_SIZE; ++keysize)
+  {
+    double distance = 0;
+    for(unsigned int i = 0; i < keysize;)
+    {
+      string1[i] = fgetc(file);
+      if(string1[i] != '\n')
+      {
+        ++i;
+      }
+    }
+    for(unsigned int i = 0; i < keysize;)
+    {
+      string2[i] = fgetc(file);
+      if(string2[i] != '\n')
+      {
+        ++i;
+      }
+    }
+    string1[keysize] = '\0';
+    string2[keysize] = '\0';
+    distance = get_hamming_distance(string1, string2) / (double) keysize;
+    printf("Keysize:%u Distance: %f\n", keysize, distance);
+    if(distance < min_distance)
+    {
+      min_distance = distance;
+      best_keysize = keysize;
+    }
+  }
+  printf("Best guess for keysize: %u\n", best_keysize);
 }

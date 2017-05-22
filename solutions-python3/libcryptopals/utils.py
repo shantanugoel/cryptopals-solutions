826f2dbb3c75a5e988986988cda1fee64878c5da
Shantanu Goel <shantanu.goel@gmail.com>
2017-05-22 22:44:37 +0530

    solution 2 in py

--- solutions-python3/libcryptopals/utils.py
+++ solutions-python3/libcryptopals/utils.py
@@ -4,3 +4,15 @@ from base64 import b64encode
 
 def hex_to_base64(hex_str):
   return str(object=b64encode(bytes.fromhex(hex_str)), encoding='utf-8')
+
+
+def fixed_xor(first_buffer, second_buffer):
+  if(len(first_buffer) != len(second_buffer)):
+    raise ValueError('Both buffers should be of same size')
+
+  output = b''
+
+  for x, y in zip(first_buffer, second_buffer):
+    output += bytes([x ^ y])
+
+  return output
